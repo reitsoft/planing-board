137102c5e22db2417a6aec01f7506962b9301da7
Alex <reit85@gmail.com>
2021-08-07 10:24:52 +0200

    comming soon page created

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders comming soon text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText(/Waaahhh, comming soon/i);
+  expect(h1Element).toBeInTheDocument();
 });
